daec0bd187741008e2c7b14361a0276dcdf31f93
MyronFanQiu <49134743+MyronFanQiu@users.noreply.github.com>
2021-08-24 10:43:11 +0800

    Adapt to crust mainnet (#1)
    * use MyronFanQiu
    
    * go update
    
    * add crust events
    
    * fix StakingLimitSuccess
    
    * fix scheduler event and use v3.0.9 GCSPR
    
    * fix offence and release v0.0.5
    
    * change MyronFanQiu to crustio

--- events.go
+++ events.go
@@ -1,7 +1,10 @@
 package events
 
 import (
-	"github.com/centrifuge/go-substrate-rpc-client/types"
+	"fmt"
+
+	"github.com/crustio/go-substrate-rpc-client/v3/types"
+	"github.com/crustio/go-substrate-rpc-client/v3/scale"
 )
 
 type Events struct {
@@ -18,6 +21,335 @@ type Events struct {
 	ChainBridge_ProposalRejected        []EventProposalRejected        //nolint:stylecheck,golint
 	ChainBridge_ProposalSucceeded       []EventProposalSucceeded       //nolint:stylecheck,golint
 	ChainBridge_ProposalFailed          []EventProposalFailed          //nolint:stylecheck,golint
+}
+
+type CrustEvents struct {
+	Staking_EraReward                       []EventEraReward                    //nolint:stylecheck,golint
+	Staking_ValidateSuccess                 []EventValidateSuccess              //nolint:stylecheck,golint
+	Staking_GuaranteeSuccess                []EventGuaranteeSuccess             //nolint:stylecheck,golint
+	Staking_CutGuaranteeSuccess             []EventCutGuaranteeSuccess          //nolint:stylecheck,golint
+	Staking_ChillSuccess                    []EventChillSuccess                 //nolint:stylecheck,golint
+	Staking_UpdateStakeLimitSuccess         []EventUpdateStakeLimitSuccess      //nolint:stylecheck,golint
+	Swork_RegisterSuccess                   []EventRegisterSuccess              //nolint:stylecheck,golint
+	Swork_WorksReportSuccess                []EventWorksReportSuccess           //nolint:stylecheck,golint
+	Swork_ABUpgradeSuccess                  []EventABUpgradeSuccess             //nolint:stylecheck,golint
+	Swork_SetCodeSuccess                    []EventSetCodeSuccess               //nolint:stylecheck,golint
+	Swork_JoinGroupSuccess                  []EventJoinGroupSuccess             //nolint:stylecheck,golint
+	Swork_QuitGroupSuccess                  []EventQuitGroupSuccess             //nolint:stylecheck,golint
+	Swork_CreateGroupSuccess                []EventCreateGroupSuccess           //nolint:stylecheck,golint
+	Swork_KickOutSuccess                    []EventKickOutSuccess               //nolint:stylecheck,golint
+	Swork_AddIntoAllowlistSuccess           []EventAddIntoAllowlistSuccess      //nolint:stylecheck,golint
+	Swork_RemoveFromAllowlistSuccess        []EventRemoveFromAllowlistSuccess   //nolint:stylecheck,golint
+	Swork_SetPunishmentSuccess              []EventSetPunishmentSuccess         //nolint:stylecheck,golint
+	Swork_RemoveCodeSuccess                 []EventRemoveCodeSuccess            //nolint:stylecheck,golint
+	Market_FileSuccess                      []EventFileSuccess                  //nolint:stylecheck,golint
+	Market_RenewFileSuccess                 []EventRenewFileSuccess             //nolint:stylecheck,golint
+	Market_AddPrepaidSuccess                []EventAddPrepaidSuccess            //nolint:stylecheck,golint
+	Market_CalculateSuccess                 []EventCalculateSuccess             //nolint:stylecheck,golint
+	Market_IllegalFileClosed                []EventIllegalFileClosed            //nolint:stylecheck,golint
+	Market_RewardMerchantSuccess            []EventRewardMerchantSuccess        //nolint:stylecheck,golint
+	Market_SetEnableMarketSuccess           []EventSetEnableMarketSuccess       //nolint:stylecheck,golint
+	Market_SetBaseFeeSuccess                []EventSetBaseFeeSuccess            //nolint:stylecheck,golint
+	Locks_UnlockStartedFrom                 []EventUnlockStartedFrom            //nolint:stylecheck,golint
+	Locks_UnlockSuccess                     []EventUnlockSuccess                //nolint:stylecheck,golint
+	Claims_SuperiorChanged                  []EventSuperiorChanged              //nolint:stylecheck,golint
+	Claims_MinerChanged                     []EventMinerChanged                 //nolint:stylecheck,golint
+	Claims_SetLimitSuccess                  []EventSetLimitSuccess              //nolint:stylecheck,golint
+	Claims_MintSuccess                      []EventMintSuccess                  //nolint:stylecheck,golint
+	Claims_Claimed                          []EventClaimsClaimed                //nolint:stylecheck,golint
+	Benefits_AddBenefitFundsSuccess         []EventAddBenefitFundsSuccess       //nolint:stylecheck,golint
+	Benefits_CutBenefitFundsSuccess         []EventCutBenefitFundsSuccess       //nolint:stylecheck,golint
+	Benefits_RebondBenefitFundsSuccess      []EventRebondBenefitFundsSuccess    //nolint:stylecheck,golint
+	Benefits_WithdrawBenefitFundsSuccess    []EventWithdrawBenefitFundsSuccess  //nolint:stylecheck,golint
+}
+
+type EventEraReward struct {
+	Phase           types.Phase
+	EraIndex        types.U32
+	AuthoringReward types.U128
+	StakingReward   types.U128
+	Topics          []types.Hash
+}
+
+type EventValidateSuccess struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	GuaranteeFee    types.U32
+	Topics          []types.Hash
+}
+
+type EventGuaranteeSuccess struct {
+	Phase           types.Phase
+	Source          types.AccountID
+	Target          types.AccountID
+	Amount          types.U128
+	Topics          []types.Hash
+}
+
+
+type EventCutGuaranteeSuccess struct {
+	Phase           types.Phase
+	Source          types.AccountID
+	Target          types.AccountID
+	Amount          types.U128
+	Topics          []types.Hash
+}
+
+
+type EventChillSuccess struct {
+	Phase           types.Phase
+	Stash           types.AccountID
+	Controller      types.AccountID
+	Topics          []types.Hash
+}
+
+type EventUpdateStakeLimitSuccess struct {
+	Phase           types.Phase
+	Number          types.U32
+	Topics          []types.Hash
+}
+
+type EventRegisterSuccess struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	PubKey          types.Bytes
+	Topics          []types.Hash
+}
+
+type EventWorksReportSuccess struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	PubKey          types.Bytes
+	Topics          []types.Hash
+}
+type EventABUpgradeSuccess struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	OldPubKey       types.Bytes
+	NewPubKey       types.Bytes
+	Topics          []types.Hash
+}
+
+type EventSetCodeSuccess struct {
+	Phase           types.Phase
+	Code            types.Bytes
+	BlockNumber     types.U32
+	Topics          []types.Hash
+}
+
+type EventJoinGroupSuccess struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	Group           types.AccountID
+	Topics          []types.Hash
+}
+
+type EventQuitGroupSuccess struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	Group           types.AccountID
+	Topics          []types.Hash
+}
+
+type EventCreateGroupSuccess struct {
+	Phase           types.Phase
+	Group           types.AccountID
+	Topics          []types.Hash
+}
+
+type EventKickOutSuccess struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	Topics          []types.Hash
+}
+
+type EventAddIntoAllowlistSuccess struct {
+	Phase           types.Phase
+	Group           types.AccountID
+	Who             types.AccountID
+	Topics          []types.Hash
+}
+
+type EventRemoveFromAllowlistSuccess struct {
+	Phase           types.Phase
+	Group           types.AccountID
+	Who             types.AccountID
+	Topics          []types.Hash
+}
+
+type EventSetPunishmentSuccess struct {
+	Phase           types.Phase
+	IsPunishment    bool
+	Topics          []types.Hash
+}
+
+type EventRemoveCodeSuccess struct {
+	Phase           types.Phase
+	Code            types.Bytes
+	Topics          []types.Hash
+}
+
+type EventFileSuccess struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	Cid             types.Bytes
+	Topics          []types.Hash
+}
+
+type EventRenewFileSuccess struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	Cid             types.Bytes
+	Topics          []types.Hash
+}
+
+type EventAddPrepaidSuccess struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	Cid             types.Bytes
+	Amount          types.U128
+	Topics          []types.Hash
+}
+
+type EventCalculateSuccess struct {
+	Phase           types.Phase
+	Cid             types.Bytes
+	Topics          []types.Hash
+}
+
+type EventIllegalFileClosed struct {
+	Phase           types.Phase
+	Cid             types.Bytes
+	Topics          []types.Hash
+}
+
+type EventRewardMerchantSuccess struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	Topics          []types.Hash
+}
+
+type EventSetEnableMarketSuccess struct {
+	Phase           types.Phase
+	IsEnable        bool
+	Topics          []types.Hash
+}
+
+type EventSetBaseFeeSuccess struct {
+	Phase           types.Phase
+	Amount          types.U128
+	Topics          []types.Hash
+}
+
+type EventUnlockStartedFrom struct {
+	Phase           types.Phase
+	StartFrom       types.U32
+	Topics          []types.Hash
+}
+
+type EventUnlockSuccess struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	UnlockAt        types.U32
+	Topics          []types.Hash
+}
+
+type EventSuperiorChanged struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	Topics          []types.Hash
+}
+
+type EventMinerChanged struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	Topics          []types.Hash
+}
+
+type EventSetLimitSuccess struct {
+	Phase           types.Phase
+	Limit           types.U128
+	Topics          []types.Hash
+}
+
+type EventMintSuccess struct {
+	Phase           types.Phase
+	EthereumTxHash  types.H256
+	EthereumAddress types.H160
+	Amount          types.U128
+	Topics          []types.Hash
+}
+
+// EventClaimsClaimed is emitted when an account claims some CRUs
+type EventClaimsClaimed struct {
+	Phase           types.Phase
+	Who             types.AccountID
+	EthereumAddress types.H160
+	Topics          []types.Hash
+}
+
+type EventAddBenefitFundsSuccess struct {
+	Phase     types.Phase
+	Who       types.AccountID
+	Amount    types.U128
+	Type      FundsType
+	Topics    []types.Hash
+}
+
+type EventCutBenefitFundsSuccess struct {
+	Phase     types.Phase
+	Who       types.AccountID
+	Amount    types.U128
+	Type      FundsType
+	Topics    []types.Hash
+}
+
+type EventRebondBenefitFundsSuccess struct {
+	Phase     types.Phase
+	Who       types.AccountID
+	Amount    types.U128
+	Type      FundsType
+	Topics    []types.Hash
+}
+
+type EventWithdrawBenefitFundsSuccess struct {
+	Phase     types.Phase
+	Who       types.AccountID
+	Amount    types.U128
+	Topics    []types.Hash
+}
+
+// DispatchClass is a generalized group of dispatch types. This is only distinguishing normal, user-triggered
+// transactions (`Normal`) and anything beyond which serves a higher purpose to the system (`Operational`).
+type FundsType struct {
+	// A swork funds type
+	IsSwork bool
+	// An market funds type
+	IsMarket bool
+}
+
+func (d *FundsType) Decode(decoder scale.Decoder) error {
+	b, err := decoder.ReadOneByte()
+	switch b {
+	case 0:
+		d.IsSwork = true
+	case 1:
+		d.IsMarket = true
+	default:
+		return fmt.Errorf("unknown FundsType enum: %v", b)
+	}
+	return err
+}
+
+//nolint:gocritic,golint
+func (d FundsType) Encode(encoder scale.Encoder) error {
+	var err error
+	if d.IsSwork {
+		err = encoder.PushByte(0)
+	} else if d.IsMarket {
+		err = encoder.PushByte(1)
+	}
+	return err
 }
 
 type EventFungibleTransfer struct {
